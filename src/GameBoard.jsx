efff8d9c6d4dc965cda825d18c2e39af274c4265
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-05-23 08:31:19 -0300

    Board game

---
+++ src/GameBoard.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updBoard = [...prevGameBoard.map((i) => [...i])];
+      updBoard[rowIndex][colIndex] = 'X';
+      return updBoard;
+    });
+  }
+
+  return (
+    <ol id='game-board'>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
